f10d6948b91e77ab58a0cfc93737d307b377176b
satackey <21271711+satackey@users.noreply.github.com>
2020-05-04 12:18:29 +0900

    classを使ってみた

--- abc166/c.py
+++ abc166/c.py
@@ -1,4 +1,5 @@
 from sys import stderr
+import json
 
 def debug(*args):
     print(*args, file=stderr)
@@ -6,44 +7,70 @@ def debug(*args):
 n_observatory_num, m_roads_num = map(int, input().split())
 hi_heights = list(map(int, input().split()))
 
-# def get_height_for(a_observaotry):
-#     return hi_heights[a_observaotry+1]
-
 ab_roads_fromto = [map(int, input().split()) for _ in range(m_roads_num)]
 
 
-observatory_x_connected_to = {}
+class ObservatoryController():
+    observatories_by_id = {}
+
+    def add(self, a_observatory):
+        self.observatories_by_id[a_observatory.id] = a_observatory
+
+    def connect_by_id(self, a_id, b_id):
+        observatory_a = self.observatories_by_id[a_id]
+        observatory_b = self.observatories_by_id[b_id]
+
+        observatory_a.connect_to(observatory_b)
+        observatory_b.connect_to(observatory_a)
+
+    
+    def count_good_observatories(self):
+        result = 0
+        for id, a_observaotry in self.observatories_by_id.items():
+            if a_observaotry.is_good():
+                result += 1
+        return result
+
+
+class Observatory():
+    id = None
+    height = None
+    connected_to = []
+
+
+    def __init__(self, id, height):
+        self.id = id
+        self.height = height
+        self.connected_to = []
+
+
+    def __repr__(self):
+        construct = json.dumps({
+            'id': self.id,
+            'height': self.height,
+        })
+        return "<Observatory '%s'>" % (construct)
+
+    def connect_to(self, a_observaotry):
+        self.connected_to.append(a_observaotry)
+    
+    def is_good(self):
+        for a_connected_observatory in list(self.connected_to):
+            # debug('comparing...', self, a_connected_observatory)
+            if self.height <= a_connected_observatory.height:
+                return False
+        return True
+
+observatoryController = ObservatoryController()
+
+for i, height in enumerate(hi_heights):
+    id = i + 1
+    observatory = Observatory(id, height)
+    observatoryController.add(observatory)
 
 for a, b in ab_roads_fromto:
-    a -= 1
-    b -= 1
-    if not a in observatory_x_connected_to:
-        observatory_x_connected_to[a] = []
-    if not b in observatory_x_connected_to:
-        observatory_x_connected_to[b] = []
+    observatoryController.connect_by_id(a, b)
 
-    observatory_x_connected_to[a].append(b)
-    observatory_x_connected_to[b].append(a)
-
-# debug(observatory_x_connected_to)
-
-result = 0
-for observatory_i, observatory_i_height in enumerate(hi_heights):
-    # observatory_i_height = get_height_for(observatory_i)
-    if not observatory_i in observatory_x_connected_to:
-        # debug('good no connection' + str(observatory_i))
-        result += 1
-        continue
-
-    good = True
-    for connected_to in observatory_x_connected_to[observatory_i]:
-        if observatory_i_height <= hi_heights[connected_to]:
-            good = False
-            # debug('not good' + str(observatory_i))
-            break
-    
-    if good:
-        # debug('good highest' + str(observatory_i))
-        result += 1
+result = observatoryController.count_good_observatories()
 
 print(result)
